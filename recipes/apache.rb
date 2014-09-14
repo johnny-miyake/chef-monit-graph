d6237eabc08691a75bd91bd6444b98682efaf95c
johnny <takuma.miyake@framgia.com>
2014-09-14 22:57:06 +0700

    fix virtual host

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -4,7 +4,6 @@ web_app "monit-graph" do
   if sn = node["monit-graph"]["server_name"] || node["fqdn"]
     server_name sn
   end
-  server_path "/monit-graph/"
-  docroot "#{node["monit-graph"]["install_dir"]}"
+  docroot "#{node["monit-graph"]["install_dir"]}/monit-graph"
   cookbook "apache2"
 end
